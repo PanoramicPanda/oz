7872060b453aaa47a990ed26a593810e5c00e96c
PanoramicPanda <uriah.h.brown@gmail.com>
2018-04-17 23:20:07 -0400

    Successfully logging in

--- DDB/pages/home_page.rb
+++ DDB/pages/home_page.rb
@@ -1,20 +1,6 @@
 class HomePage < DDBRootPage
   add_id_element(:link, /READ MORE/, xpath: '/html/body/div[1]/div/div[2]/div/section/div/div[1]/div[1]/div/div/ul/li[1]/article/div/div[3]/div[4]/a')
 
-  def create_elements
-    login_elements
-
-  end
-
-  def login_elements
-    # username = add_text_field(:username, id: 'username')
-    username = add_text_field(:username, xpath: '//*[@id="username"]')
-    password = add_text_field(:password, id: 'password')
-    submit_log_in = add_button(:submit_log_in, xpath: '//*[@id="loginForm"]/div[3]/button')
-    @login_elements = group(username, password, submit_log_in)
-    @login_elements.deactivate
-  end
-
   def on_page_load
     super
     perform_sign_in unless @world.ledger.logged_in
@@ -22,11 +8,9 @@ class HomePage < DDBRootPage
 
   def perform_sign_in
     click_on(:log_in_button)
-    @login_elements.activate
-    @world.browser.switch.frame('passport') # TODO: Switch to this iFrame....GRRR
-    fill('TWITCH_LOGIN')
-    click_on(:submit_log_in_button)
-    @login_elements.deactivate
+    @world.browser.iframe(:name => 'passport').text_field(:id => 'username').set 'PLACEHOLDER - Never stick this on GitHub/Anywhere Public'
+    @world.browser.iframe(:name => 'passport').text_field(:name => 'password').set 'PLACEHOLDER - Seriously - you should not stick this online'
+    @world.browser.iframe(:name => 'passport').button(:xpath => '//*[@id="loginForm"]/div[3]/button').click
     @world.ledger.save_object(:logged_in, true)
   end
 
